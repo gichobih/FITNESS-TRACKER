9e470a61f387fe94396b4ad3e6b9eb7c81cc6e2f
gichobih <dennisgichobih@gmail.com>
2025-01-23 11:47:09 +0300

    Set up Redux store with persistence using redux-persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -12,26 +12,32 @@ import {
 import storage from "redux-persist/lib/storage";
 import userReducer from "./reducers/userSlice";
 
+// Configuration for redux-persist
 const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
+  key: "root", // Key for the root of the persisted state
+  version: 1, // Version of the persisted state
+  storage, // Storage engine (localStorage)
 };
 
+// Combine all reducers (only 'user' reducer in this case)
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
+// Create a persisted reducer using the persist configuration
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Create and configure the Redux store
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: persistedReducer, // Use the persisted reducer
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
+        // Ignore specific redux-persist actions during serializable check
         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
 
+// Create a persistor instance to manage persistence
 export const persistor = persistStore(store);
